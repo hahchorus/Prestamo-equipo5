1f27984059df25800dd68e10ae6181d92788e81c
hahchorus <horusalex919.hahc@gmail.com>
2022-05-21 15:37:46 -0500

    inicio

---
+++ Prestamo/src/main/java/mx/uv/t4is/Prestamo/PrestamoApplication.java
@@ -0,0 +1,13 @@
+package mx.uv.t4is.Prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamoApplication.class, args);
+	}
+
+}
---
+++ Prestamo/src/test/java/mx/uv/t4is/Prestamo/PrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.t4is.Prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
