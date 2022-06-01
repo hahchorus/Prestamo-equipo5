694e58fd561f6a916b1a98613bbef0f6746745fc
hahchorus <horusalex919.hahc@gmail.com>
2022-06-01 01:43:09 -0500

    T3MetodoCompleto

--- Prestamo/src/main/java/mx/uv/t4is/Prestamo/IPrestamo.java
+++ Prestamo/src/main/java/mx/uv/t4is/Prestamo/IPrestamo.java
@@ -3,6 +3,6 @@ package mx.uv.t4is.Prestamo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IPrestamo extends CrudRepository<Prestamo,Integer>{
-    Iterable<Prestamo> findByIdAndTitulolibro (int id, String titulolibro);
-    Iterable<Prestamo> findByIdAndCliente (int id, String nomcliente);
+    //Iterable<Prestamo> findByIdAndTitulolibro (int id, String titulolibro);
+    //Iterable<Prestamo> findByIdAndCliente (int id, String nomcliente);
 }
